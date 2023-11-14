817cb1cee30c7fc8088c28724aeed8b0dda4cde5
Vignesh V <150780090+vignesh-ibm@users.noreply.github.com>
2023-11-14 18:51:50 +0530

    REMOVED CHART

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,7 @@ import {
 } from '@/app/lib/data';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
+  //const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
@@ -34,7 +34,6 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
